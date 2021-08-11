20ba6f34a9ffb2d4a5e4ed8d26fa745d8adada0e
Nicolai Mainiero <nicolai@mainiero.de>
2021-08-11 15:34:07 +0200

    Enable PublicKeyAuthenticatio

--- build_it_yourself/entrypoint.sh
+++ build_it_yourself/entrypoint.sh
@@ -1,11 +1,15 @@
 #! /bin/bash
 
+mkdir -p /home/user/.ssh
+touch /home/user/.ssh/authorized_keys
+
 public_keys=`ls /docker-entrypoint.d/*.pub`
 for eachfile in $public_keys
 do
    cat $eachfile >> /home/user/.ssh/authorized_keys
 done
 
+chown -R user:sftp-users /home/user/.ssh/
 chmod 0600 /home/user/.ssh/authorized_keys
 
 service ssh start && bash
