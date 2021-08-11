f6bead627751788a585e386bfad949c33e680840
Nicolai Mainiero <nicolai@mainiero.de>
2021-08-11 15:08:53 +0200

    Added option to add an public key for the default account

--- build_it_yourself/entrypoint.sh
+++ build_it_yourself/entrypoint.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
+public_keys=`ls /docker-entrypoint.d/*.pub`
+for eachfile in $public_keys
+do
+   cat $eachfile >> /home/user/.ssh/authorized_keys
+done
+
+chmod 0600 /home/user/.ssh/authorized_keys
+
 service ssh start && bash
