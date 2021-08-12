bc0eb57bd555ec359ab8fbe23b3c7e79b20c3674
Nicolai Mainiero <nicolai@mainiero.de>
2021-08-12 12:04:33 +0200

    Changed accessible folder on sftp

--- build_it_yourself/default_user.sh
+++ build_it_yourself/default_user.sh
@@ -6,6 +6,6 @@ readonly password='password'
 
 useradd $user -s /usr/sbin/nologin -g sftp-users -m && \
 echo $user:$password | chpasswd && \
-chmod 700 /home/$user && setfacl -dRm u::7,g::0,o::0 /home/$user && mkdir -p /home/$user/$user-files && \
-chown -R $user:sftp-users /home/$user/$user-files/ && setfacl -dRm u::7,g::0,o::0 /home/$user/$user-files && \
+chmod 700 /home/$user && setfacl -dRm u::7,g::0,o::0 /home/$user && mkdir -p /home/$user/export && \
+chown -R $user:sftp-users /home/$user/export/ && setfacl -dRm u::7,g::0,o::0 /home/$user/export && \
 chown root:root /home/$user/ && chmod 755 /home/$user
